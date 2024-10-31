5593563968ff7d3bc3faa362e16544021d064f9c
Xyrfo <rofo.anggi18@gmail.com>
2024-10-31 22:18:40 +0700

    percobaan 2

--- ArrayBilangan26.java
+++ ArrayBilangan26.java
@@ -3,6 +3,7 @@ public class ArrayBilangan26 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
         int[] bil = new int[4];
 
         bil[0] = 5;
